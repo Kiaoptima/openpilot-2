8adeb81855059627907cf4a98ef1c55d8abbfd72
Kiaoptima <47659862+Kiaoptima@users.noreply.github.com>
2021-08-20 09:19:08 +0200

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -13,7 +13,7 @@ class CarControllerParams:
     else:
       self.STEER_MAX = 384 # default 384
       self.STEER_DELTA_UP = 3 # default 3
-      self.STEER_DELTA_DOWN = 3 # default 7
+      self.STEER_DELTA_DOWN = 4 # default 7
     self.STEER_DRIVER_ALLOWANCE = 50
     self.STEER_DRIVER_MULTIPLIER = 2
     self.STEER_DRIVER_FACTOR = 1
